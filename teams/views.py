0940e2a6159fce4bfed8edc7e3980a8eaf7e9163
Gustavo Luz <gustavohlluz2@gmail.com>
2024-01-09 18:53:58 -0300

    tarefas finalizadas

--- teams/views.py
+++ teams/views.py
@@ -1,4 +1,5 @@
 from django.forms import model_to_dict
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView, Request, Response, status
 from teams.models import Team
 from utils import data_processing
@@ -27,7 +28,35 @@ class TeamView(APIView):
 
 
 class TeamDetailView(APIView):
-    ...
+     def get(self, request: Request, team_id) -> Response:
+        try:
+            found_team = Team.objects.get(pk=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
 
+        team_dict = model_to_dict(found_team)
+        return Response(team_dict, status.HTTP_200_OK)
+    
+     def patch(self, request: Request, team_id) -> Response:
+        try:
+            found_team = Team.objects.get(pk=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+        for key,value in request.data.items():
+            setattr(found_team,key,value)
+            found_team.save()
+            team_dict = model_to_dict(found_team)
 
-
+        return Response(team_dict, status.HTTP_200_OK)
+    
+     def delete(self, request: Request, team_id) -> Response:
+        try:
+            found_team = Team.objects.get(pk=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+        found_team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+     
+     
+    
+    
